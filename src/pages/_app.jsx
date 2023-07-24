672736e2ed9bc88d40e6078dbef945fcfbcc6eb0
Niels Kieviet <info@nielskieviet.nl>
2023-07-24 16:24:08 +0200

    use GA id

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,7 @@ import { NextGA } from 'nextgas'
 export default function App({ Component, pageProps }) {
   return (
       <>
-        <NextGA mId={`${process.env.NEXT_GA}`} />
+        <NextGA mId={"G-5B4KVZ5YCY"} />
         <Component {...pageProps} />
       </>
   )
